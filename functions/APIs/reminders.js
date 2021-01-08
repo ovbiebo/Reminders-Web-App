a35906bb7a5dd18f10f14dfcc4db5e63e6278fc4
Chidubem Ovbiebo <chidubemovbiebo@gmail.com>
2021-01-08 12:45:14 +0100

    Added cloud functions for adding, deleting and updating reminder documents

--- functions/APIs/reminders.js
+++ functions/APIs/reminders.js
@@ -1,3 +1,4 @@
+const {firestore} = require("firebase-admin/lib/firestore");
 const {database} = require('../utils/admin')
 
 exports.getAllReminders = (request, response) => {
@@ -18,6 +19,66 @@ exports.getAllReminders = (request, response) => {
                 return response.json(reminders);
             }
         )
+        .catch((err) => {
+            console.error(err);
+            return response.status(500).json({error: err.code});
+        });
+}
+
+exports.addReminder = (request, response) => {
+    if (request.body.subject.trim() === '') {
+        return response.status(400).json({body: 'Must not be empty'});
+    }
+
+    const newReminder = {
+        Start: firestore.Timestamp.fromDate(new Date()),
+        End: firestore.Timestamp.fromDate(new Date()),
+        Subject: request.body.subject
+    }
+
+    database.collection("Reminders")
+        .add(newReminder)
+        .then((docRef) => {
+            const responseReminder = newReminder;
+            responseReminder.id = docRef.id;
+            return response.json(responseReminder);
+        })
+        .catch((err) => {
+            console.error(err);
+            return response.status(500).json({error: err.code});
+        });
+}
+
+exports.deleteReminder = (request, response) => {
+
+    const document = database.collection("Reminders").doc(request.params.reminderId);
+
+    document
+        .get()
+        .then((docRef) => {
+            if (!docRef.exists) {
+                return response.status(404).json({error: 'Reminder not found'});
+            }
+            return document.delete();
+        })
+        .then(() => {
+            response.json({message: 'Delete successful'});
+        })
+        .catch((err) => {
+            console.error(err);
+            return response.status(500).json({error: err.code});
+        });
+}
+
+exports.editReminder = (request, response) => {
+
+    const document = database.collection("Reminders").doc(request.params.reminderId);
+
+    document
+        .set(request.body, {merge: true})
+        .then(() => {
+            response.json({message: 'Update successful'});
+        })
         .catch((err) => {
             console.error(err);
             return response.status(500).json({error: err.code});
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
-const app = require('express')();
+const express = require('express');
+const app = express();
 
-const {getAllReminders} = require('./APIs/reminders');
+const {getAllReminders, addReminder, deleteReminder, editReminder} = require('./APIs/reminders');
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get("/reminders", getAllReminders);
+app.post("/reminder", addReminder);
+app.delete("/reminder/:reminderId", deleteReminder);
+app.put("/reminder/:reminderId", editReminder);
 
 exports.api = functions.https.onRequest(app);

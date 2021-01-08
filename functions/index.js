ebeb6b5b5e391ea3faa5df92d1919057c3003603
Chidubem Ovbiebo <chidubemovbiebo@gmail.com>
2021-01-08 19:40:56 +0100

    firebase installed

--- functions/index.js
+++ functions/index.js
@@ -4,13 +4,13 @@ const app = express();
 
 const {getAllReminders, addReminder, deleteReminder, editReminder} = require('./APIs/reminders');
 
-// middleware
-app.use(express.json());
-app.use(express.urlencoded());
-
 app.get("/reminders", getAllReminders);
 app.post("/reminder", addReminder);
 app.delete("/reminder/:reminderId", deleteReminder);
 app.put("/reminder/:reminderId", editReminder);
+
+const {loginUser} = require('./APIs/users');
+
+app.post("/login", loginUser);
 
 exports.api = functions.https.onRequest(app);
